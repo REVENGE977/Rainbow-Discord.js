b197f51869acfbe0a4680341e1a57d841499de79
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-12-22 08:12:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,6 +40,7 @@ console.log('Done The Watching Setup Completed')
 });
 //Best Rainbow Bot .
 client.on('message', message => {//new msg event
+	if(!message.guild.channel) return;
   if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
 	  let role = message.guild.roles.find('name', 'Rainbow bot.')
     if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
@@ -93,6 +94,7 @@ The steps of the role did not worked .!!
 message.author.sendEmbed(rainembed)//send the embed to the author dm
     }})
    client.on('message', message => {//new cmd
+	   	if(!message.guild.channel) return;
 	   if(message.content.startsWith(`r#inv`)) { //the invite bot cmd
 		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")//if he type the cmd in the bot dm return with this msg
                  message.react('🌈')
