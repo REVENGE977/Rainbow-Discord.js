947380b741a0447de62cd153a1e86140a38c5f45
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-12-22 07:32:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,105 @@
 
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = 'r#'
+//information about the bot
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[═════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations About Rainbow bot:')
+  console.log('')
+  console.log(`Servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`Channels! [ " ${client.channels.size} " ]`);
+  console.log(`Arch! [ " ${process.arch} " ]`);
+  console.log(`Platform! [ " ${process.platform} " ]`);
+  console.log(`Node Version! [ " ${process.version}" ]`);
+  console.log(`Prefix! [ " ${prefix}" ]`);
+  console.log(`Language! [ " NodeJS " ]`);
+  console.log(`Ram Usage! [ " ${(process.memoryUsage().rss / 1048576).toFixed()}MB " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(`${client.user.username} Is Online`)
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log('Created By: Revenge')
+  console.log('╚[════════════]╝')
+});
+//Best Rainbow Bot .
+client.on('message', message => {//new msg event
+let role = message.guild.roles.find('name', 'Rainbow bot.')
+  if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow bot.",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
+
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', 'Rainbow bot.');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 5000);//the rainbow time
+})
+
+client.on("message", message => {//new msg event
+  if (message.content === "r#help") {//the help cmd
+      message.react('🌈')
+        let rainembed = new Discord.RichEmbed()//new embed
+        //the embed description (help msg)
+        .setDescription(`**
+=====================🌈 RainbowBot. 🌈=====================
+r#set 
+ - لإنشاء رتبة الرينبو وبدا الرينبو
+- To create the role of the Rainbow & Start The Rainbow
+r#inv 
+- لدعوة البوت
+- To Invite the bot
+خطوات لو الرتبة م أشتغلت .!!
+1- ضع رتبة الرينبو فوق الالوان أو الرتب الملونه لو فيه
+2- ضع رتبة البوت فوق رتبة الرينبو
+The steps of the role did not worked .!!
+1- Place the role of the Rainbow above the colors or colored ranks if it
+2- Put the bot role above the role of the Rainbow 
+رآبط البوت - Bot Invite link
+- https://discordapp.com/oauth2/authorize?client_id=" + ${client.user.id} + "&scope=bot&permissions=2080374975
+=====================🌈 RainbowBot. 🌈=====================
+**`)
+message.author.sendEmbed(rainembed)//send the embed to the author dm
+    }})
+   client.on('message', message => {//new cmd
+	   if(message.content.startsWith(`r#inv`)) { //the invite bot cmd
+		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")//if he type the cmd in the bot dm return with this msg
+                 message.react('🌈')
+		   var embed = new Discord.RichEmbed()
+		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
+		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "&scope=bot&permissions=2080374975")
+		   .setTimestamp()
+		   .setFooter(`Requested By | ${message.author.username}`)
+		   .setColor("RANDOM")
+		   message.author.send({embed})//send the url in the author dm
+	   }
+   });
+client.login(process.env.BOT_TOKEN)
